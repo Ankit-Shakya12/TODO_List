e011ee8d0f78083aa9bb9bd24b7565de3001b180
Ankit Shakya <159711353+Ankit-Shakya12@users.noreply.github.com>
2024-09-15 13:49:33 +0530

    Update models.py

--- todolist/models.py
+++ todolist/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class toDolist(models.Model):
     tasks = models.CharField(max_length=50)
     complete = models.BooleanField(default=False)
-    create_at = models.DateTimeField(auto_now_add=True)
+    create_at = models.DateTimeField()
 
     def __str__(self):
         return self.tasks
